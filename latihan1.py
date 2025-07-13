8795e2da6841f7581026f934db04cfd2e5c65af6
raghibhabibi <raghibhabibi.study@gmail.com>
2025-07-13 10:23:19 +0700

    latihan1

--- latihan1.py
+++ latihan1.py
@@ -1,5 +1,8 @@
-print('Halo')
-print("halo2")
-awal = 0
-untung = 2
-print(awal+untung)
+'''
+Program yang mencetak semua bilangan dari 1 sampai n,
+tapi hanya tampilkan yang kelipatan 3 atau 5.
+'''
+n = int(input("Masukkan nomor : "))
+for i in range(1,n+1):
+    if i%3==0 or i%5==0:
+        print(i)
