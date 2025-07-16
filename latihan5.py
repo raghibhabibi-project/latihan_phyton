10942be50b2bce86e1251e6134de46a163485863
raghibhabibi <raghibhabibi.study@gmail.com>
2025-07-16 20:40:04 +0700

    latihan

--- latihan5.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-pd.set_option("display.max_columns", 50)
-
-table = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/penduduk_gender_head.csv")
-table.head()
-print(table)
---
+++ latihan6.py
@@ -0,0 +1,4 @@
+file = open("file.txt", "r")
+file.write("ini adalah ini file nya")
+
+file.close()
