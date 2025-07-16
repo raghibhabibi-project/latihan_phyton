abfc85d7f161afec577e54632dca4279cc892268
raghibhabibi <raghibhabibi.study@gmail.com>
2025-07-16 23:14:30 +0700

    latihan

---
+++ latihan4.py
@@ -0,0 +1,10 @@
+list = ["line 1\n", "line 2\n", "line 3\n"]
+file = open("file.txt", "a")
+
+#file.write("ini adalah ini file nya\n")
+
+#print(file.readlines()[])
+
+file.write("\nIni line baru")
+
+file.close()
--- latihan6.py
+++
@@ -1,4 +0,0 @@
-file = open("file.txt", "r")
-file.write("ini adalah ini file nya")
-
-file.close()
