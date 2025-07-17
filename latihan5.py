9fdd7685a4ea725bef2f634de469b915fc2958b3
raghibhabibi <raghibhabibi.study@gmail.com>
2025-07-18 00:24:03 +0700

    latihan

---
+++ latihan5.py
@@ -0,0 +1,9 @@
+import csv
+list = ["Nama", "Umur", "Asal"]
+datalist = [["andi", "30", "Jogja"], ["Budi", "35", "Solo"],["Burhan", "25", "Bogor"]]
+with open("data.csv", "w", newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    writer.writerow(list)
+    writer.writerows(datalist)
+    
+
---
+++ latihan6.py
@@ -0,0 +1,12 @@
+import csv
+list = ["Nama", "Umur", "Asal"]
+datadic = [
+    {"Nama" : "Andi", "Umur" : "30", "Asal" : "Jogja"},
+    {"Nama" : "Budi", "Umur" : "35", "Asal" : "Solo"},
+    {"Nama" : "Burhan", "Umur" : "25", "Asal" : "Bogor"}
+    ]
+with open("data2.csv", "w", newline='', encoding='utf-8') as file:
+    writer = csv.DictWriter(file, fieldnames=list)
+    writer.writeheader()
+    writer.writerows(datadic)
+
