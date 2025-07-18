d6f517ece24bdcf8545dc2b70848687ed1ad5540
raghibhabibi <raghibhabibi.study@gmail.com>
2025-07-18 21:59:32 +0700

    project

--- latihan7.py
+++ latihan7.py
@@ -18,10 +18,41 @@ def tambah_peserta():
     peserta_asal.append(asal)
 def tampilkan_peserta():
     print("\nDaftar Peserta: ")
-    jumlah = len(nama)
+    jumlah = len(peserta_nama)
     if jumlah == 0:
         print("Belum ada peserta terdaftar")
     else:
         for i in range(jumlah):
-            print(f"{i+1}. Nama: {peserta_nama[i]}, Umur: {peserta_umur[i]}, Asal: {peserta_asal{i}}")
-    
+            print(f"{i+1}. Nama: {peserta_nama[i]} - Umur: {peserta_umur[i]} - Asal: {peserta_asal[i]}")
+def simpan_csv():
+    with open("peserta_workshop.csv", "w", newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Nama", "Umur", "Asal"])
+        for i in range(len(peserta_nama)):
+            writer.writerow([peserta_nama[i], peserta_umur[i], peserta_asal[i]])
+            print("Data Peserta Berhasil Disimpan")
+def baca_csv():
+        with open("peserta_workshop.csv", "r", newline='', encoding='utf-8') as file:
+            reader = csv.reader(file)
+            next(reader)
+            for row in reader:
+                peserta_nama.append(row[0])
+                peserta_umur.append(row[1])
+                peserta_asal.append(row[2])
+            print("Data berhasil dibaca")
+while True:
+    tampilan_menu()
+    pilihan = input("Pilih menu : ")
+    if pilihan == "1":
+        tambah_peserta()
+    elif pilihan == "2":
+        tampilkan_peserta()
+    elif pilihan == "3":
+        simpan_csv()
+    elif pilihan == "4":
+        baca_csv()
+    elif pilihan == "0":
+        print("Terima kasih telah berkunjung")
+        break
+    else:
+        print("Pilihan tidak valid. Coba lagi!")
